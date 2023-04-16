e5f67a8dbee17a1b5be1aa3d82e6170651fc6b98
ekzyis <ekzyis@ekzyis.com>
2023-04-16 17:53:49 +0200

    Check for dupes before posting

--- go.mod
+++ go.mod
@@ -3,6 +3,10 @@ module gitlab.com/ekzyis/hnbot
 go 1.20
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/namsral/flag v1.7.4-pre // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.8.2 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- sn.go
+++ sn.go
@@ -13,6 +13,18 @@ type GraphQLPayload struct {
 	Variables map[string]interface{} `json:"variables,omitempty"`
 }
 
+type Dupe struct {
+	Id    int    `json:"id,string"`
+	Url   string `json:"url"`
+	Title string `json:"title"`
+}
+
+type DupesResponse struct {
+	Data struct {
+		Dupes []Dupe `json:"dupes"`
+	} `json:"data"`
+}
+
 func filterByRelevanceForSN(stories *[]Story) *[]Story {
 	// TODO: filter by relevance
 
@@ -20,8 +32,55 @@ func filterByRelevanceForSN(stories *[]Story) *[]Story {
 	return &slice
 }
 
+func fetchDupes(url string) *[]Dupe {
+	body := GraphQLPayload{
+		Query: `
+			query Dupes($url: String!) {
+				dupes(url: $url) {
+					id
+					url
+					title
+				}
+			}`,
+		Variables: map[string]interface{}{
+			"url": url,
+		},
+	}
+
+	bodyJSON, err := json.Marshal(body)
+	if err != nil {
+		log.Fatal("Error during json.Marshal:", err)
+	}
+
+	apiUrl := "https://stacker.news/api/graphql"
+	req, err := http.NewRequest("POST", apiUrl, bytes.NewBuffer(bodyJSON))
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Cookie", fmt.Sprintf("__Host-next-auth.csrf-token=%s", SnApiToken))
+
+	client := http.DefaultClient
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	var dupesResp DupesResponse
+	err = json.NewDecoder(resp.Body).Decode(&dupesResp)
+	if err != nil {
+		log.Fatal("Error decoding dupes JSON:", err)
+	}
+
+	return &dupesResp.Data.Dupes
+}
+
 func postToSN(story *Story) {
-	// TODO: check for dupes first
+	dupes := fetchDupes(story.Url)
+	if len(*dupes) > 0 {
+		return
+	}
 
 	body := GraphQLPayload{
 		Query: `
@@ -42,8 +101,8 @@ func postToSN(story *Story) {
 		log.Fatal("Error during json.Marshal:", err)
 	}
 
-	url := "https://stacker.news/api/graphql"
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(bodyJSON))
+	apiUrl := "https://stacker.news/api/graphql"
+	req, err := http.NewRequest("POST", apiUrl, bytes.NewBuffer(bodyJSON))
 	if err != nil {
 		panic(err)
 	}
@@ -57,5 +116,5 @@ func postToSN(story *Story) {
 	}
 	defer resp.Body.Close()
 
-	log.Printf("POST %s %d\n", url, resp.StatusCode)
+	log.Printf("POST %s %d\n", apiUrl, resp.StatusCode)
 }
