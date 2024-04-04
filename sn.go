ee95aa89bd5c291d0a62217a981628e6aa796cc9
ekzyis <ekzyis@ekzy.is>
2024-04-04 20:22:36 +0200

    Remove chart link and cron.sh
    I think it was more annoying than interesting to look at.
    
    Also, it didn't seem to be worth the maintenance of the cronjob etc.

--- sn.go
+++ sn.go
@@ -94,7 +94,7 @@ func PostStoryToStackerNews(story *Story, options PostStoryOptions) (int, error)
 		humanize.Time(time.Unix(int64(story.Time), 0)),
 		HackerNewsItemLink(story.ID),
 		story.Score, story.Descendants,
-	) + fmt.Sprintf("\n\nhttps://files.ekzyis.com/public/hn/hn_%d.png", story.ID)
+	)
 	if _, err := sn.CreateComment(parentId, comment); err != nil {
 		return -1, fmt.Errorf("error posting comment :%w", err)
 	}
